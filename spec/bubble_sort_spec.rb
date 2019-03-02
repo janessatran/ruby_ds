54d5ef1802b45c545cf008c3b24516125c760f9a
Janessa Tran <tranjanessa@gmail.com>
2019-03-01 21:41:28 -0600

    add changes based on PR review

--- spec/bubble_sort_spec.rb
+++ spec/bubble_sort_spec.rb
@@ -5,7 +5,6 @@ describe '#bubble_sort' do
   it 'compares each element in an array, swaps them, until the array is sorted' do
     expect((bubble_sort([4,5,2,1,6]))).to eq([1,2,4,5,6])
   end
-
   it 'returns the array if the array size is 1' do
     expect((bubble_sort([1]))).to eq([1])
   end
@@ -14,11 +13,14 @@ end
 
 describe '#bubble_sort_by' do
 
+  # we can use the yield function to change functionality of the sort
+  # this is useful for comparing objects, since the comparison
+  # would depend on something the creator defines
   it 'uses the yield function' do
     result = bubble_sort_by(['hello', 'hi', 'hey']) do |left, right|
       left.length - right.length
     end
     expect(result).to eq(['hi', 'hey', 'hello'])
-  end 
+  end
  
 end

d7606277ed62b3c01d907035f328631e3653eb2b
Janessa Tran <tranjanessa@gmail.com>
2019-03-03 11:11:04 -0600

    removed bubble sort methods

--- my_enumerable_module.rb
+++ my_enumerable_module.rb
@@ -97,38 +97,6 @@ module Enumerable
       idx += 1
     end
     accum
-  end 
-
-
-  def bubble_sort(arr)
-    return if arr.length == 1
-    swapped = true
-    while swapped
-      swapped = false
-      (0..arr.length-2).each do |idx|
-        if arr[idx] > arr[idx+1]
-          arr[idx], arr[idx+1] = arr[idx+1], arr[idx]
-          swapped = true
-        end
-      end
-    end
-    arr
-  end
-  
-  def bubble_sort_by(arr)
-    return if arr.length == 1
-    swapped = true
-    while swapped
-      swapped = false
-      (0..arr.length-2).each do |idx|
-        comparison = yield arr[idx], arr[idx+1]
-        if comparison > 0
-          arr[idx], arr[idx+1] = arr[idx+1], arr[idx]
-          swapped = true
-        end
-      end
-    end
-    arr
   end
 
 end

9bd10d18cc94bf6ea9e864df182015a42b6f4836
Janessa Tran <tranjanessa@gmail.com>
2019-07-02 19:46:16 -0500

    Add linked list implementation and update spacing in spec

--- spec/bubble_sort_spec.rb
+++ spec/bubble_sort_spec.rb
@@ -1,14 +1,12 @@
-require ('./bubble_sort')
+require './bubble_sort'
 
 describe '#bubble_sort' do
-
   it 'compares each element in an array, swaps them, until the array is sorted' do
-    expect((bubble_sort([4,5,2,1,6]))).to eq([1,2,4,5,6])
+    expect(bubble_sort([4, 5, 2, 1, 6])).to eq([1, 2 , 4, 5, 6])
   end
   it 'returns the array if the array size is 1' do
     expect((bubble_sort([1]))).to eq([1])
   end
-  
 end
 
 describe '#bubble_sort_by' do
@@ -22,5 +20,4 @@ describe '#bubble_sort_by' do
     end
     expect(result).to eq(['hi', 'hey', 'hello'])
   end
- 
 end
